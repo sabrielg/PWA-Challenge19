a500de770aebe882b6c91be4e5160f04d8d3b693
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-03-12 11:44:10 -0700

    added to scripts

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,6 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_budget', { autoIncrement: true });
   };
 
@@ -76,4 +75,4 @@ function saveRecord(record) {
     // more to come...
   }
 
-  window.addEventListener('online', uploadPizza);
+  window.addEventListener('online', uploadBudget);
